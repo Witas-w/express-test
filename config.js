5ad3b2e3c7b56dc1e4de66740a5d8264849cab83
witas <witas.w@poczta.fm>
2022-08-29 20:01:39 +0200

    commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://witas:rpnIXUSvgKSmD51y@cluster0.nxbgos1.mongodb.net/?retryWrites=true&w=majority',
+    keySession: ['KeyAdmin'],
+    maxAgeSession: 24 * 60 * 60 * 1000,
+}
